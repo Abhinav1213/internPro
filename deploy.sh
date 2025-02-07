19e379d525487d062a6a3f9c8b10130e302c91af
Abhinav1213 <113532240+Abhinav1213@users.noreply.github.com>
2025-02-07 11:20:27 +0530

    Deployment code

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r dist/* -i C:\Users\abhin\.ssh\skylink root@167.71.229.65:/var/www/167.71.229.65/
+scp -r dist/* -i "C:\Users\abhin\.ssh\skylink" root@167.71.229.65:/var/www/167.71.229.65/
+
 
 echo "Done!"
